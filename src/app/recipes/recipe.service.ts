ed09bc0bfa588492df46b2bca105d8dd8b5bf1b7
Vaibhav Gadge <39002899+profile4vaibhav@users.noreply.github.com>
2022-09-27 14:41:03 +0530

    Passed ingredients from recipes to shopping list via a service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,15 +1,42 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
 
+@Injectable()
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'Test Recipe Description', 'https://i.guim.co.uk/img/media/c63a7652c379e866a04877bcdf44e0d3e8f1aadb/0_366_4278_5195/master/4278.jpg?width=1300&quality=85&fit=max&s=08bf81c2c0e9c879fcb8c2ae21e72584'),
-        new Recipe('Another Test Recipe', 'Test Recipe Description', 'https://i.guim.co.uk/img/media/c63a7652c379e866a04877bcdf44e0d3e8f1aadb/0_366_4278_5195/master/4278.jpg?width=1300&quality=85&fit=max&s=08bf81c2c0e9c879fcb8c2ae21e72584'),
+        new Recipe(
+            'A Test Recipe',
+            'Test Recipe Description',
+            'https://i.guim.co.uk/img/media/c63a7652c379e866a04877bcdf44e0d3e8f1aadb/0_366_4278_5195/master/4278.jpg?width=1300&quality=85&fit=max&s=08bf81c2c0e9c879fcb8c2ae21e72584',
+            [
+                new Ingredient('Veggies', 1),
+                new Ingredient('Fries', 20),
+            ]
+        ),
+        new Recipe(
+            'Another Test Recipe',
+            'Test Recipe Description',
+            'https://static.toiimg.com/thumb/83565509.cms?width=1200&height=900',
+            [
+                new Ingredient('Buns', 1),
+                new Ingredient('Potato', 1),
+            ]
+        ),
     ];
+
+    constructor(
+        private shoppingListService: ShoppingListService
+    ) { }
 
     getRecipes() {
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.shoppingListService.addIngredients(ingredients);
     }
 }
