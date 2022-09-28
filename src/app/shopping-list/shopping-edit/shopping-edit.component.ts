2cc7f8af64af069aa019b45e0114d651cd42d653
Vaibhav Gadge <39002899+profile4vaibhav@users.noreply.github.com>
2022-09-28 10:24:30 +0530

    Allowed the selection of ingredients list item in the list

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Subject, takeUntil } from 'rxjs';
 import { Ingredient } from 'src/app/shared/ingredient.model';
+import { threadId } from 'worker_threads';
 import { ShoppingListService } from '../shopping-list.service';
 
 @Component({
@@ -8,19 +10,32 @@ import { ShoppingListService } from '../shopping-list.service';
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
+export class ShoppingEditComponent implements OnInit, OnDestroy {
+  endSubs$: Subject<any> = new Subject();
+  editMode: boolean = false;
+  editedItemIndex: number;
 
   constructor(
     private shoppingListService: ShoppingListService
   ) { }
 
   ngOnInit(): void {
+    this.shoppingListService.startedEditing.pipe(takeUntil(this.endSubs$)).subscribe((index: number) => {
+      if (index) {
+        this.editMode = true;
+        this.editedItemIndex = index;
+      }
+    })
   }
 
   onAddItem(form: NgForm) {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
     this.shoppingListService.addIngredient(newIngredient);
+  }
+
+  ngOnDestroy(): void {
+    this.endSubs$.complete();
   }
 
 }
