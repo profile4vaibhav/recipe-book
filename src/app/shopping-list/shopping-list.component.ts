355458033950c6ea4187e3c6ea065b71774c8512
Vaibhav Gadge <39002899+profile4vaibhav@users.noreply.github.com>
2022-09-28 10:03:10 +0530

    Improving reative service with observables (Subject)

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
 
@@ -7,8 +8,9 @@ import { ShoppingListService } from './shopping-list.service';
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
+  endSubs$: Subject<any> = new Subject();
 
   constructor(
     private shoppingListService: ShoppingListService
@@ -16,9 +18,13 @@ export class ShoppingListComponent implements OnInit {
 
   ngOnInit(): void {
     this.ingredients = this.shoppingListService.getIngredients();
-    this.shoppingListService.ingredientsChanged.subscribe((ingredients: Ingredient[]) => {
+    this.shoppingListService.ingredientsChanged.pipe(takeUntil(this.endSubs$)).subscribe((ingredients: Ingredient[]) => {
       this.ingredients = ingredients;
     })
+  }
+
+  ngOnDestroy(): void {
+    this.endSubs$.complete();
   }
 
 }
